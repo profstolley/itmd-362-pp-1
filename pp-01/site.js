ce91e908703a4ba62671c45149fbf9d56e7067ae
Karl Stolley <karl.stolley@gmail.com>
2018-01-30 17:04:50 -0600

    Placeholder function definitions

--- pp-01/site.js
+++ pp-01/site.js
@@ -3,3 +3,15 @@ $('#user').on('submit', function(e) {
   $('#primary').append('<p class="success">Thank you. Your information has been saved.</p>');
   e.preventDefault();
 });
+
+function is_probably_phone(phone) {
+
+}
+
+function is_probably_email(email) {
+
+}
+
+function is_eighteen(birthday) {
+
+}
